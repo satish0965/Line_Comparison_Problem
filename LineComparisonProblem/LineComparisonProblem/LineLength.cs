f82a1e1a1a3dbb0bb25f6d548a59caf8912b80f8
Satish Patil <satish.patil7017@gmail.com>
2022-08-21 19:03:32 +0530

    [Satish] Added Calculating Length Solution

---
+++ LineComparisonProblem/LineComparisonProblem/LineLength.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparisonProblem
+{
+    public class LineLength
+    {
+        int X1;
+        int Y1;
+        int X2;
+        int Y2;
+        double Length_XY;
+        public void CalculateLength()
+        {
+            Console.WriteLine("Enter X,Y Co_Ordinates of the Point 1: ");
+            Console.Write("Enter Value X1: ");
+            X1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Value Y1: ");
+            Y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter X,Y Co_Ordinates of the Poin 2: ");
+            Console.Write("Enter Value X2: ");
+            X2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Value Y2: ");
+            Y2 = Convert.ToInt32(Console.ReadLine());
+            Length_XY = Math.Sqrt(Math.Pow((X2 - X1), 2) + Math.Pow((Y2 - Y1), 2));
+            Console.WriteLine("Length of Value is : " + Length_XY);
+            Console.WriteLine("==================================================================================");
+
+
+        }
+    }
+}
---
+++ LineComparisonProblem/LineComparisonProblem/Program.cs
@@ -0,0 +1,15 @@
+﻿//Console.WriteLine("Welcome to Line Comparision Computation Problem");
+using System;
+namespace LineComparisonProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("==========Welcome To Line Comparison Computation Program==========");
+            LineLength length = new LineLength();
+            length.CalculateLength(); //Call The Methods
+            Console.ReadLine();
+        }
+    }
+}
